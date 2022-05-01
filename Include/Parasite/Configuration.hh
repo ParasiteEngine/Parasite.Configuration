bcc20301f85e6a470f5bdcd607ec40dcb5a50bf6
deathblizzard <me.deathblizzard@gmail.com>
2022-05-01 11:20:55 +0530

    Add minimal CMake configuration and header files Fixes #1
    *   Added CHANGELOG.md
    *   Added CMakeLists.txt
    *   Added CMakeSources.txt
    *   Added Parasite/Configuration.hh
    *   Added Parasite/Configuration/Version.hh.in

---
+++ Include/Parasite/Configuration.hh
@@ -0,0 +1,46 @@
+//  Parasite::Configuration
+//  Copyright 2022 DeathBlizzard
+//  
+//  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+//  conditions are met:
+//  
+//  1.  Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+//      disclaimer.
+//  
+//  2.  Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+//      disclaimer in the documentation and/or other materials provided with the distribution.
+//  
+//  3.  Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products
+//      derived from this software without specific prior written permission.
+//  
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+//  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+//  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#ifndef PARASITE_ENGINE__CONFIGURATION_HH_
+#define PARASITE_ENGINE__CONFIGURATION_HH_
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file       Parasite/Configuration.hh
+/// @brief      This file includes all the necessary headers that are part of the Parasite::Configuration module.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @defgroup   parasite_configuration  Parasite::Configuration
+/// @brief      The Parasite::Configuration module
+/// @details    This module encapsulates platform, compiler, language standard, CMake modules and other prerequisites for
+///             Parasite Engine by means defining macros, compilation flags and `constexpr` functions allowing other modules and
+///             client sources to utilize and develop lnaguage standard, compiler and platform independent code base.
+/// @{
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <Parasite/Configuration/Version.hh>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#endif
